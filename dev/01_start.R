11c653834cf97b4a35df617f6d16eb32a8044334
sandpiles <blanch2008@gmail.com>
2024-05-27 22:51:38 +0200

    small changes

--- dev/01_start.R
+++ dev/01_start.R
@@ -22,9 +22,9 @@ golem::fill_desc(
   pkg_name = "massspecgolem", # The Name of the package containing the App
   pkg_title = "PKG_TITLE", # The Title of the package containing the App
   pkg_description = "PKG_DESC.", # The Description of the package containing the App
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
+  author_first_name = "Jordi", # Your First Name
+  author_last_name = "Martinez Blanch", # Your Last Name
+  author_email = "blanch@gmail.com", # Your Email
   repo_url = NULL, # The URL of the GitHub Repo (optional),
   pkg_version = "0.0.0.9000" # The Version of the package containing the App
 )
